dd237e1a0bb415bae23d9a968331876b4b3d7898
pkellz <patrickscott2013@gmail.com>
2021-06-20 23:29:57 -0400

    Add dynamic property

---
+++ Javascript/DynamicProperty.js
@@ -0,0 +1,15 @@
+/*
+    Polyfills -> https://www.youtube.com/watch?v=NdIfAdIo290
+    Author: DevSage (Youtube) -> https://www.youtube.com/DevSage
+*/
+
+const ltc = "litecoin"
+
+const cryptos = {
+    bitcoin: 35000,
+    [ltc]: 150
+    // litcoin: 150
+}
+
+console.log(cryptos.bitcoin)
+console.log(cryptos.litecoin)
