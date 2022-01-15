7d403dc57f9ef78fd50a7190d4ecd12b0fffda95
pkellz <patrickscott2013@gmail.com>
2022-01-14 19:24:17 -0500

    Add retry pattern

---
+++ DesignPatterns/Retry.js
@@ -0,0 +1,44 @@
+async function retryOperation()
+{
+    let currentTry = 0;
+
+    while(true)
+    {
+        try {
+            externalServiceCall();
+            console.log("Succeeded!");
+            break;
+        } catch (error) {
+            currentTry++;
+            console.log(`Failed attempt ${currentTry}`);
+
+            if(currentTry >= 10)
+            {
+                console.log("Retry maximum reached. Exiting");
+                break;
+            }
+        }
+        await sleep(3000);
+    }  
+}     
+
+function externalServiceCall()
+{
+    console.log("Calling external service...");
+    const shouldPass = Math.random() < 0.4;
+    if(shouldPass)
+        return true;
+    else
+        throw "Failure";
+}
+
+async function sleep(ms)
+{
+    return new Promise(resolve => {
+        setTimeout(() => {
+            resolve()
+        }, ms);
+    });
+}
+
+retryOperation();
